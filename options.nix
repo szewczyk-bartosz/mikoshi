48c1f985296ea9d9c192c0d03efed8d562faa59b
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-15 23:56:06 +0000

    updated options for flake parts and added an enable option for hyprland

--- options.nix
+++ options.nix
@@ -1,18 +1,24 @@
-lib: {
-  theme = lib.mkOption {
-    default = "akasara";
-    type = lib.types.str;
-    description = "the theme that Mikoshi will use currently not implemented"; # TODO: Implement themes
-  };
-  monitor = lib.mkOption {
-    default = ",preferred,auto,auto";
-    type = lib.types.str;
-    description = "Hyprland monitor configuration (format: 'name,resolution,position,scale')";
-  };
+{lib, ...}: {
+  flake.nixosModules.options = {lib, ...}: {
+    options.mikoshi = {
+      hyperland.enable = lib.mkEnableOption "Mikoshi's hyprland setup";
 
-  keyboardLayout = lib.mkOption {
-    default = "gb";
-    type = lib.types.str;
-    description = "Keyboard layout to use";
+      theme = lib.mkOption {
+        default = "akasara";
+        type = lib.types.str;
+        description = "the theme that Mikoshi will use currently not implemented"; # TODO: Implement themes
+      };
+      monitor = lib.mkOption {
+        default = [",preferred,auto,auto"];
+        type = lib.types.listOf lib.types.str;
+        description = "A list of Hyprland monitor configurations (format: 'name,resolution,position,scale')";
+      };
+
+      keyboardLayout = lib.mkOption {
+        default = "gb";
+        type = lib.types.str;
+        description = "Keyboard layout to use";
+      };
+    };
   };
 }
