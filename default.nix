ba049395f54ad738855f8a66051cd7d7a765f81c
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-16 01:20:27 +0000

    yeah that doesn't work

--- default.nix
+++ default.nix
@@ -9,9 +9,6 @@
       self.nixosModules.hyprland
     ];
 
-    # Apply the home-manager configuration to all non-root users
-    home-manager.users = lib.mapAttrs (username: _: {
-      imports = [self.homeManagerModules.hyprland];
-    }) (lib.filterAttrs (username: _: username != "root") config.users.users);
+    home-manager.sharedModules = [self.homeManagerModules.hyprland];
   };
 }
