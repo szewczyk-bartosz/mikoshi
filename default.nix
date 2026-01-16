54084e773a36269feac27c1e24d28a39b01627a9
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-16 00:52:42 +0000

    home-manager application to non-root users

--- default.nix
+++ default.nix
@@ -8,5 +8,10 @@
       self.nixosModules.options
       self.nixosModules.hyprland
     ];
+
+    # Apply the home-manager configuration to all non-root users
+    home-manager.users = lib.mapAttrs (username: _: {
+      imports = [self.homeManagerModules.hyprland];
+    }) (lib.filterAttrs (username: _: username != "root") config.users.users);
   };
 }
