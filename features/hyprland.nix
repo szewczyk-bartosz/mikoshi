c17b4fa476d36058e883d291aefe131f5e8c2407
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-19 20:07:58 +0000

    fix

--- features/hyprland.nix
+++ features/hyprland.nix
@@ -7,9 +7,8 @@
     imports = [self.nixosModules.options];
     config = lib.mkIf config.mikoshi.hyprland.enable {
       programs.hyprland.enable = true;
+      home-manager.sharedModules = [self.homeManagerModules.hyprland];
     };
-
-    home-manager.sharedModules = [self.homeManagerModules.hyprland];
   };
 
   flake.homeManagerModules.hyprland = {
