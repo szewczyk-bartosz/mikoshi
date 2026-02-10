449f1516c7d8c89cf2b3f3d9adbbda83a9a644f8
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-02-10 12:15:34 +0000

    bunch of options that will be split into their own features eventually

--- features/gnome/system.nix
+++ features/gnome/system.nix
@@ -7,7 +7,30 @@
   }: {
     imports = [self.modules.nixos.gnomeOptions];
     config = lib.mkIf config.mikoshi.gnome.enable {
+      # TODO: This entire section should be a seperate feature
+      i18n.defaultLocale = "en_US.UTF-8";
+      console = {
+        font = "Lat2-Terminus16";
+        useXkbConfig = true;
+      };
+
+      networking.networkmanager.enable = true;
+      services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+      };
+      environment.variables = {
+        GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}";
+      };
+
+      programs.dconf.enable = true;
+
+      ##########
+
       services.displayManager.gdm.enable = true;
+      services.xserver.enable = true;
+      time.timeZone = "Europe/London"; #TODO: Make this an option
+      services.xserver.xkb.layout = config.gnome.kb;
       services.desktopManager.gnome.enable = true;
       services.gnome.core-apps.enable = false;
       programs.seahorse.enable = true;
