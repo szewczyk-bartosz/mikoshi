3be9d75d2207ce0d2df562a93fb66af5e165dbe0
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-02-28 02:22:41 +0000

    updated devtools

--- features/devTools/system.nix
+++ features/devTools/system.nix
@@ -8,13 +8,17 @@
     # imports = [self.modules.nixos.gnomeOptions];
     # TODO: Add options to this
     config = {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
       environment.systemPackages = with pkgs; [
         wget
         git
         rustc
         cargo
         rustfmt
-        (python313.withPackages (packages: with packages; [lxml requests beautifulsoup4 pandas]))
+        (python313.withPackages (packages: with packages; [lxml requests tqdm beautifulsoup4 pandas]))
         maven
         jdk
       ];
