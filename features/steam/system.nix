8440caccada281efd74153ca91fcd56945000269
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-10 21:14:18 +0000

    game mode added

--- features/steam/system.nix
+++ features/steam/system.nix
@@ -11,6 +11,7 @@
         enable32Bit = true;
       };
       programs.steam.enable = true;
+      programs.gamemode.enable = true;
 
       environment.systemPackages = with pkgs; [
         protonup-qt
