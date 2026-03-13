4473e3ad0bb76feff81a8229d6cedd0b3a9dff69
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-13 22:06:07 +0000

    updated tmux with new keybinds

--- features/tmux/home.nix
+++ features/tmux/home.nix
@@ -10,5 +10,12 @@
     historyLimit = 10000;
     focusEvents = true; # For neovim to work correctly
     terminal = "tmux-256color"; # COLOUR
+    extraConfig = ''
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      bind a last-window
+    '';
   };
 }
