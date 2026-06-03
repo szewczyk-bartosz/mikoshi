9ec181cb71675acd2e4b645d4108752cce96fa04
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-06-03 20:59:06 +0100

    trying to fix hyprland

--- features/hyprland/system.nix
+++ features/hyprland/system.nix
@@ -73,7 +73,16 @@ in {
       useXkbConfig = lib.mkDefault true;
     };
 
-    services.displayManager.gdm.enable = true;
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+          user = "greeter";
+        };
+      };
+    };
+
     environment.systemPackages = with pkgs; [
       daemon
       switcher
