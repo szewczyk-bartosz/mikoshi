d707a3ee6b93c5c4b40ef5a15a3646d4a126cb2f
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 00:11:05 +0100

    tmux done

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,17 +1,17 @@
 {...}: {
-  flake.modules.homeManager.base = {
+  flake.modules.homeManager.base = {lib, ...}: {
     programs.tmux = {
-      enable = true;
-      prefix = "C-Space";
-      keyMode = "vi";
-      clock24 = true;
-      escapeTime = 0;
-      mouse = true;
-      baseIndex = 1;
-      historyLimit = 10000;
-      focusEvents = true; # For neovim to work correctly
-      terminal = "tmux-256color"; # COLOUR
-      extraConfig = ''
+      enable = lib.mkDefault true;
+      prefix = lib.mkDefault "C-Space";
+      keyMode = lib.mkDefault "vi";
+      clock24 = lib.mkDefault true;
+      escapeTime = lib.mkDefault 0;
+      mouse = lib.mkDefault true;
+      baseIndex = lib.mkDefault 1;
+      historyLimit = lib.mkDefault 10000;
+      focusEvents = lib.mkDefault true; # For neovim to work correctly
+      terminal = lib.mkDefault "tmux-256color"; # COLOUR
+      extraConfig = lib.mkDefault ''
         bind h select-pane -L
         bind j select-pane -D
         bind k select-pane -U
