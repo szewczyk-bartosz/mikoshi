04019cee98edf1727d9dcde43b60a4295e775277
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-08 23:58:52 +0100

    upgrades to networking

--- modules/network.nix
+++ modules/network.nix
@@ -1,5 +1,15 @@
 {...}: {
   flake.modules.nixos.base = {
+    lib,
+    config,
+    ...
+  }: {
+    assertions = [
+      {
+        assertion = config.networking.hostName != "";
+        message = "networking.hostName must be set";
+      }
+    ];
     networking.networkmanager.enable = true;
   };
 }
