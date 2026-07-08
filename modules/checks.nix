d3a5299198cdd8601ae28b8c6fc9cc29ca549b96
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-08 22:45:30 +0100

    ok checks are now working

--- modules/checks.nix
+++ modules/checks.nix
@@ -1,30 +1,28 @@
 {
-  inputs,
   config,
+  inputs,
   ...
 }: {
-  perSystem = {
-    system,
-    pkgs,
-    ...
-  }: let
-    testSystem = inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        config.flake.modules.nixos.base
-        {
-          mikoshi.users = ["testuser"];
-          users.users.testuser.isNormalUser = true;
-          home-manager.users.testuser.home.stateVersion = "26.05";
-          system.stateVersion = "26.05";
-          boot.loader.grub.enable = false;
-          boot.isContainer = true;
-        }
-      ];
-    };
-  in {
-    checks.mikoshi-check = pkgs.runCommand "check-mikoshi" {} ''
-      echo ${testSystem.config.system.build.toplevel.drvPath} > $out
+  flake.nixosConfigurations.ci-test = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      config.flake.modules.nixos.base
+      {
+        mikoshi.users = ["testuser"];
+        users.users.testuser.isNormalUser = true;
+        home-manager.users.testuser.home.stateVersion = "25.05";
+        system.stateVersion = "25.05";
+        boot.isContainer = true;
+      }
+    ];
+  };
+
+  perSystem = {pkgs, ...}: {
+    checks.mikoshi-eval-only = pkgs.runCommand "mikoshi-eval-only" {} ''
+      echo ${
+        builtins.unsafeDiscardOutputDependency
+        config.flake.nixosConfigurations.ci-test.config.system.build.toplevel.drvPath
+      } > $out
     '';
   };
 }
---
+++ modules/tmux.nix
@@ -0,0 +1,23 @@
+{...}: {
+  flake.modules.homeManager.base = {
+    programs.tmux = {
+      enable = true;
+      prefix = "C-Space";
+      keyMode = "vi";
+      clock24 = true;
+      escapeTime = 0;
+      mouse = true;
+      baseIndex = 1;
+      historyLimit = 10000;
+      focusEvents = true; # For neovim to work correctly
+      terminal = "tmux-256color"; # COLOUR
+      extraConfig = ''
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+        bind a last-window
+      '';
+    };
+  };
+}
