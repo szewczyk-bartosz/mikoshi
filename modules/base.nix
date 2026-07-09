c93a58334f804d2b184450a584d1b6c7f1089f2d
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 12:58:13 +0100

    a little bug from copy pasting

--- modules/base.nix
+++ modules/base.nix
@@ -1,11 +1,15 @@
 {config, ...}: {
-  flake.modules.nixos.base = {lib, ...}: {
+  flake.modules.nixos.base = {
+    lib,
+    config,
+    ...
+  }: {
     console = {
       font = lib.mkDefault "Lat2-Terminus16";
       useXkbConfig = lib.mkDefault true;
     };
     i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
     time.timeZone = lib.mkDefault "Europe/London";
-    services.xserver.xkb.layout = lib.mkDefault (lib.concatStringsSep "," config.mikoshi.gnome.kb);
+    services.xserver.xkb.layout = lib.mkDefault (lib.concatStringsSep "," config.mikoshi.keyboardLayouts);
   };
 }
