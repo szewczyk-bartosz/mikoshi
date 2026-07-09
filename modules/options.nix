e9c4a9efeeed9041ba20d889fc392d11bf665786
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 11:16:52 +0100

    keyboardLayouts added to global mikoshi settings

--- modules/options.nix
+++ modules/options.nix
@@ -1,9 +1,16 @@
 {...}: {
   flake.modules.nixos.base = {lib, ...}: {
-    options.mikoshi.users = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [];
-      description = "Users to configure via home-manager on this host";
+    options.mikoshi = {
+      users = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [];
+        description = "Users to configure via home-manager on this host";
+      };
+      keyboardLayouts = {
+        default = ["gb"];
+        type = lib.types.listOf lib.types.str;
+        description = "the keyboard layouts to set";
+      };
     };
   };
 }
