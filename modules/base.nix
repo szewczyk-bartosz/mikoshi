98adcbc18106787d2118cc94a39609b80ff988ab
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 11:16:22 +0100

    moved tty settings, timezones and locale to a base.nix file

---
+++ modules/base.nix
@@ -0,0 +1,11 @@
+{config, ...}: {
+  flake.modules.nixos.base = {lib, ...}: {
+    console = {
+      font = lib.mkDefault "Lat2-Terminus16";
+      useXkbConfig = lib.mkDefault true;
+    };
+    i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+    time.timeZone = lib.mkDefault "Europe/London";
+    services.xserver.xkb.layout = lib.mkDefault (lib.concatStringsSep "," config.mikoshi.gnome.kb);
+  };
+}
