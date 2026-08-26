dd0d1cdf57966835b0af121e3a97e7dd97d1c00d
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-08-27 00:42:28 +0100

    formatting

--- modules/base.nix
+++ modules/base.nix
@@ -2,28 +2,37 @@
   inputs,
   config,
   ...
-}: let
+}:
+let
   hmFor = config.flake.lib.hmFor;
   hmClass = config.flake.modules.homeManager;
-in {
-  flake.modules.nixos.base = {
-    config,
-    lib,
-    ...
-  }: {
-    imports = [inputs.home-manager.nixosModules.home-manager];
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users = hmFor config.mikoshi.meta.users hmClass.base;
+in
+{
+  flake.modules.nixos.base =
+    {
+      config,
+      lib,
+      ...
+    }:
+    {
+      imports = [ inputs.home-manager.nixosModules.home-manager ];
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users = hmFor config.mikoshi.meta.users hmClass.base;
 
-    nix.settings.experimental-features = ["nix-command" "flakes"];
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
-    console = {
-      font = lib.mkDefault "Lat2-Terminus16";
-      useXkbConfig = lib.mkDefault true;
+      console = {
+        font = lib.mkDefault "Lat2-Terminus16";
+        useXkbConfig = lib.mkDefault true;
+      };
+      i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+      time.timeZone = lib.mkDefault "Europe/London";
+      services.xserver.xkb.layout = lib.mkDefault (
+        lib.concatStringsSep "," config.mikoshi.meta.keyboardLayouts
+      );
     };
-    i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
-    time.timeZone = lib.mkDefault "Europe/London";
-    services.xserver.xkb.layout = lib.mkDefault (lib.concatStringsSep "," config.mikoshi.meta.keyboardLayouts);
-  };
 }
